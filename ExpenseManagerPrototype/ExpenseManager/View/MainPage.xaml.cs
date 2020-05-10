86d17f56fc2c0c8f206516e309a5664d2c902c85
Francesco Lopez <francesco.lopez92@hotmail.com>
2020-05-10 20:34:12 +0930

    Initial Prototype Loading.

---
+++ ExpenseManagerPrototype/ExpenseManager/View/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ExpenseManager.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            menuPage.listView.ItemSelected += OnItemSelected;
+        }
+
+        void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var item = e.SelectedItem as Model.MasterPageItem;
+            if (item != null)
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(item.TargetType));
+                menuPage.listView.SelectedItem = null;
+                IsPresented = false;
+            }
+        }
+    }
+}
