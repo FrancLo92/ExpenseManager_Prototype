f920aded74d3e5a9ce8fb3bb1bc24ab19f68fa3f
Samara Lock <locksamara@gmail.com>
2020-09-28 14:46:59 +0930

    Implementation of LocateUs & RequestAppointment

--- ExpenseManagerPrototype/ExpenseManager/View/LocateUs.xaml.cs
+++ ExpenseManagerPrototype/ExpenseManager/View/LocateUs.xaml.cs
@@ -14,7 +14,26 @@ namespace ExpenseManager.View
     {
         public LocateUs()
         {
-            InitializeComponent();
+            WebView webView = new WebView
+            {
+                Source = new UrlWebViewSource
+                {
+                    Url = "https://thekalculators.com.au/locations/",
+                },
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+
+            // Accomodate iPhone status bar.
+            //this.Padding = new Thickness(10, Device.OnPlatform(20, 0, 0), 10, 5);
+
+            // Build the page.
+            this.Content = new StackLayout
+            {
+                Children =
+                {
+                    webView
+                }
+            };
         }
     }
 }
