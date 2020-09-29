f448506a37b3cf4a83f831087c81789921633263
Francesco <francesco.lopez92@hotmail.com>
2020-09-29 16:14:00 +0930

    Added start up page as it wasn't included in the last commit.

---
+++ ExpenseManagerPrototype/ExpenseManager/View/StartUp.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ExpenseManager.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StartUp : ContentPage
+    {
+        public StartUp()
+        {
+            InitializeComponent();
+        }
+
+        private async void loginSignupBtn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new View.LoginPage());
+        }
+
+        private void requestAppointmentBtn_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new RequestAppointment());
+        }
+
+        private void locateUsBtn_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new LocateUs());
+        }
+    }
+}
